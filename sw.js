5827cd8e4bebdd3eeb52715d844c7c6b7e04b8a6
AImagic <ai_magic@MacBook-Air-Ila.local>
2026-03-30 09:49:23 +0700

    v25: add security meta tags (nosniff, no-referrer, format-detection)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'deepwork-v24';
+const CACHE = 'deepwork-v25';
 const ASSETS = ['./', './index.html'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
